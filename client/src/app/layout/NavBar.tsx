fa786d2ba97e1507dd5120b9cefa9d66282cc73c
suawek013 <sojkaslawek123@gmail.com>
2022-05-12 01:31:23 +0200

    add Routes

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
-import { useStore } from "../stores/store";
 
 export default function NavBar() {
-	const { activityStore } = useStore();
-
 	return (
 		<Menu inverted fixed="top">
 			<Container>
-				<Menu.Item header>
+				<Menu.Item header as={NavLink} to="/">
 					<img
 						src="/assets/logo.png"
 						alt="logo"
@@ -16,11 +14,14 @@ export default function NavBar() {
 					/>
 					Reactivites
 				</Menu.Item>
+				<Menu.Item as={NavLink} to="/activities" name="Activities"></Menu.Item>
+
 				<Menu.Item>
 					<Button
-						onClick={() => activityStore.openForm()}
 						positive
 						content="Create Activity"
+						as={NavLink}
+						to="createActivity"
 					/>
 				</Menu.Item>
 			</Container>
--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,19 +1,24 @@
 import { observer } from "mobx-react-lite";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import { v4 as uuid } from "uuid";
 
 export default observer(function ActivityForm() {
+	const history = useHistory();
 	const { activityStore } = useStore();
 	const {
-		selectedActivity,
-		closeForm,
 		createActivity,
 		updateActivity,
 		loading,
+		loadingInitial,
+		loadActivity,
 	} = activityStore;
+	const { id } = useParams<{ id: string }>();
 
-	const initialState = selectedActivity ?? {
+	const [activity, setActivity] = useState({
 		id: "",
 		title: "",
 		category: "",
@@ -21,12 +26,26 @@ export default observer(function ActivityForm() {
 		date: "",
 		venue: "",
 		city: "",
-	};
+	});
 
-	const [activity, setActivity] = useState(initialState);
+	useEffect(() => {
+		if (id) loadActivity(id).then((activity) => setActivity(activity!));
+	}, [id, loadActivity]);
 
 	function handleSubmit() {
-		activity.id ? updateActivity(activity) : createActivity(activity);
+		if (activity.id.length === 0) {
+			let newActivity = {
+				...activity,
+				id: uuid(),
+			};
+			createActivity(activity).then(() =>
+				history.push(`/activities/${newActivity.id}`)
+			);
+		} else {
+			updateActivity(activity).then(() =>
+				history.push(`/activities/${activity.id}`)
+			);
+		}
 	}
 
 	function handleInputChange(
@@ -35,6 +54,8 @@ export default observer(function ActivityForm() {
 		const { name, value } = event.target;
 		setActivity({ ...activity, [name]: value });
 	}
+
+	if (loadingInitial) return <LoadingComponent content="Loading activity..." />;
 
 	return (
 		<Segment clearing>
@@ -84,11 +105,11 @@ export default observer(function ActivityForm() {
 					content="Submit"
 				/>
 				<Button
-					onClick={closeForm}
 					floated="right"
-					positive
 					type="button"
 					content="Cancel"
+					as={Link}
+					to="/activities"
 				/>
 			</Form>
 		</Segment>
